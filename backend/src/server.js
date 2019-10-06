68691a374fc432855fb5ed278b66a3025c43bc26
Italo <italoaurelior@gmail.com>
2019-10-06 10:22:02 -0300

    Add env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+require('dotenv').config()
 
 const routes = require('./routes');
 
 //Criar aplicação
 const app = express();
-const uri = 'mongodb+srv://tattoour:7OivkRjdV2PjV1wk@tattour-m5yy0.mongodb.net/tattoour?retryWrites=true&w=majority'
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@tattour-m5yy0.mongodb.net/tattoour?retryWrites=true&w=majority`
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
